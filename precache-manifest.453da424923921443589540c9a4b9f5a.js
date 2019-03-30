cc3a667138415fb4dc8ad4c235aabeaef14df024
Marcus Alfredsson <marcus@alfredo.se>
2019-03-30 18:43:41 +0100

    Updates

---
+++ precache-manifest.453da424923921443589540c9a4b9f5a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "2239894702dd8211588f7203ee3546c9",
+    "url": "/weather-app/static/media/sunny.22398947.jpg"
+  },
+  {
+    "revision": "32a8591d6dcfb8986e12bcd3ee269c58",
+    "url": "/weather-app/static/media/snow.32a8591d.jpg"
+  },
+  {
+    "revision": "f7b1bb6d1a363615eda86a0a2df7e249",
+    "url": "/weather-app/static/media/rain.f7b1bb6d.jpg"
+  },
+  {
+    "revision": "5256e3d141c6db51dfc8609b02d168ff",
+    "url": "/weather-app/static/media/night.5256e3d1.jpg"
+  },
+  {
+    "revision": "0679f82da1a462f7758d27cc32b16620",
+    "url": "/weather-app/static/media/clouds.0679f82d.jpg"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "f5b305223211c7d3cde0",
+    "url": "/weather-app/static/js/main.9bcfbca3.chunk.js"
+  },
+  {
+    "revision": "108ffa34793d7a6427e3",
+    "url": "/weather-app/static/js/2.851f3b40.chunk.js"
+  },
+  {
+    "revision": "f5b305223211c7d3cde0",
+    "url": "/weather-app/static/css/main.57d39dd8.chunk.css"
+  },
+  {
+    "revision": "cd624d3d87fd79dd71fc057defb7167d",
+    "url": "/weather-app/index.html"
+  }
+];
